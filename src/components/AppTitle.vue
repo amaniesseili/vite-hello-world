6ff0c40834c49dd4f1ad1227be70276a50d803a6
amani esseili <esseiliamani@hotmail.com>
2023-08-27 00:30:11 +0200

    apptitle completed

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,9 +1,17 @@
-<template>
-  <h1> La mia prima app con Vite !</h1>
-</template>
-
 <script>
 export default {
   name:"AppTitle"
 }
 </script>
+
+<template>
+  <h1> La mia prima app con Vite !</h1>
+</template>
+
+
+<style scoped>
+h1{
+  font-size: 50px;
+
+}
+</style>
